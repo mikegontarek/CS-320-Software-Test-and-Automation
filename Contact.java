e9d44960c90487b3ed910f05a22fcc661e538f4d
mikegontarek <30638867+mikegontarek@users.noreply.github.com>
2022-08-04 01:19:25 -0500

    Add files via upload
    Created Main method with a Switch statement to display a console log for users to interact with. Also created a displayAll method to display the bids the user enters into the array list.

--- Contact.java
+++ Contact.java
@@ -1,4 +1,6 @@
 package Contact;
+import java.util.Scanner;
+
 
 public class Contact {
 	//Declare contact variables
@@ -7,6 +9,7 @@ public class Contact {
 		private String lastName;
 		private String phoneNumber;
 		private String address;
+		
 		
 		public Contact(String contactId, String firstName, String lastName, String phoneNumber, String address) {
 			if(contactId == null || contactId.length() > 10) {
@@ -31,45 +34,110 @@ public class Contact {
 			this.phoneNumber = phoneNumber;
 			this.address = address;
 		}
+		
+		public static void main(String[] args) {
+			
+			Scanner scanner = new Scanner(System.in);
+			ContactService CS= new ContactService();
+			int choice = 0;
+			
+			// While loop that displays the menu for the user to choose from. 
+			while(choice !=3) {
+				System.out.println("Menu: ");
+				System.out.println("1. Enter Contact");
+				System.out.println("2. Display Contacts");
+				System.out.println("3. Exit");
+				System.out.println("Enter a Choice");
+				choice=scanner.nextInt();
+				
+				// Switch statements that user chooses from the menu.
+				switch(choice) {
+				case 1:
+					System.out.println("Enter Contact ID:");
+					String contactId = scanner.next();
+
+					System.out.println("Enter First Name:");
+					String firstName = scanner.next();
+					
+					System.out.println("Enter Last Name: ");
+					String lastName = scanner.next();
+				
+					System.out.println("Enter Phone Number: ");
+					String phoneNumber = scanner.next();
+					
+					System.out.println("Enter Address: ");
+					String address = scanner.next();
+					address = address + scanner.nextLine();
+					
+					// Display contact information that was just entered. 
+					
+					System.out.println("Contact ID; " + contactId);
+					System.out.println("First Name: " + firstName);
+					System.out.println("Last Name: " + lastName);
+					System.out.println("Phone Number: " + phoneNumber);
+					System.out.println("Address: " + address);
+					
+					// Add contact into the array list
+					Contact contact = new Contact(contactId, firstName, lastName, phoneNumber, address);
+					
+					// Call addContact to add to array list
+					CS.addContact(contact);
+					break;
+					
+				case 2:
+					// If two print Display Contacts
+					System.out.println("Display Contacts");
+					
+					// Call displayContacts to display the contact List
+					CS.displayContacts();
+					break;
+					
+				}
+			}
+			// If three print Good Bye!
+			System.out.println("Good Bye!");
+			
+	
+		}
+		
 		//GETTER METHODS
-		//getter method to return contactId
+		
 		public String getContactId() {
 			return contactId;
 		}
-		//getter method to return firstName
+		
 		public String getFirstName() {
 			return firstName;
 		}
-		//getter method to return lastName
+		
 		public String getLastName() {
 			return lastName;
 		}
-		//getter method to return phoneNumber
+		
 		public String getPhoneNumber() {
 			return phoneNumber;
 		}
-		//getter method to return address
+		
 		public String getAddress() {
 			return address;
 		}
 		
+		
 		//SETTER METHODS
 		 
-		//setter method to set firstName
 		public void setFirstName(String firstName) {
 			this.firstName = firstName;
 		}
-		//setter method to set lastName
+		
 		public void setLastName(String lastName) {
 			this.lastName = lastName;
 		}		
-		//setter method to set phoneNumber
+
 		public void setPhoneNumber(String phoneNumber) {
 			this.phoneNumber = phoneNumber;
 		}
-		//setter method to set address
+		
 		public void setAddress(String address) {
 			this.address = address;
 		}
-		
 }
--- ContactService.java
+++ ContactService.java
@@ -2,6 +2,8 @@ package Contact;
 
 import java.util.ArrayList;
 
+
+
 public class ContactService {
 	//Declare ArrayList to hold contacts
 	public ArrayList<Contact> contactList;
@@ -15,6 +17,8 @@ public class ContactService {
         contactList = new ArrayList<Contact>();
         contactList.add(contact);
     }
+	
+	
 	//Method to add new contact if not existing
 	public boolean addContact(Contact contact) {
         if(lookupContact(contact.getContactId()) != null) {
@@ -23,6 +27,8 @@ public class ContactService {
         contactList.add(contact);
         return true;
     }
+	
+	
 	//Method to remove a contact
 	public boolean deleteContact(String contactId) {		///ID
         Contact retrieved = lookupContact(contactId);				///ID
@@ -52,6 +58,7 @@ public class ContactService {
 
         return false;
 	}
+	
 	//Method to update phone number
 	public boolean updatePhoneNumber(String contactId, String phoneNumber) {			///ID
         Contact retrieved = lookupContact(contactId);										///ID
@@ -61,7 +68,8 @@ public class ContactService {
         }
 
         return false;
-    }
+	}
+	
 	//Method to update address
 	public boolean updateAddress(String contactId, String address) {						///ID		
         Contact retrieved = lookupContact(contactId);									///id
@@ -72,8 +80,9 @@ public class ContactService {
 
         return false;
     }
+	
 	//Method to lookup contact
-	public Contact lookupContact(String contactId) {									///ID
+	public Contact lookupContact(String contactId) {									//ID
         for(Contact contact : contactList) {
             if(contactId.equals(contact.getContactId())) {					///beginning only  ID
                 return contact;
@@ -81,6 +90,13 @@ public class ContactService {
         }
 
         return null;
+	}
+	
+	//Method to display all contacts in the arrayList
+	public void displayContacts() {
+		for(Contact contact : contactList) {
+			System.out.println(contact.getContactId() + ", " + contact.getFirstName() + ", " + contact.getLastName() + ", " + contact.getPhoneNumber() + ", " + contact.getAddress());
+		}
 	}
 }
 
